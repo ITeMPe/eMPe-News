2c5a7d8b94e2a2dd7a17db2778b316b4a274a4a7
Marcin Przepiórkowski <przepiorkowski.marcin@gmail.com>
2020-03-19 23:45:15 +0100

    task2: Small corrections for endpoint

--- src/main/java/mp/empe/news/controller/NewsController.java
+++ src/main/java/mp/empe/news/controller/NewsController.java
@@ -6,16 +6,17 @@ import org.springframework.web.client.RestTemplate;
 
 import javax.servlet.http.HttpServletRequest;
 
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/")
 public class NewsController {
 
+    static final String key = d5cb1c65bb3442048ecba183138ceaef;
 
     @GetMapping("/headlines/{country_id}/{category_id}")
     public String printWelcome(@PathVariable("country_id") String country_id, @PathVariable("category_id") String category_id) {
         RestTemplate restTemplate = new RestTemplate();
-        String url = "http://newsapi.org/v2/top-headlines?country="+country_id+"&category="+category_id+"&apiKey=d5cb1c65bb3442048ecba183138ceaef";
+        String url = "http://newsapi.org/v2/top-headlines?country="+country_id+"&category="+category_id+"&apiKey="+key;
         return restTemplate.getForObject(url, String.class);
     }
 
