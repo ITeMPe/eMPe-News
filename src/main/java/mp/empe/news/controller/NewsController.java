6fcee8e83aa5a08271f7e81e7d9dcf508d0b482d
Marcin Przepiórkowski <przepiorkowski.marcin@gmail.com>
2020-03-23 00:48:42 +0100

    task_5: Small code refactor for backend

--- src/main/java/mp/empe/news/controller/NewsController.java
+++ src/main/java/mp/empe/news/controller/NewsController.java
@@ -1,23 +1,25 @@
 package mp.empe.news.controller;
 
-
-import org.json.JSONArray;
-import org.json.JSONException;
 import org.json.JSONObject;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
-import java.util.*;
-import javax.servlet.http.HttpServletRequest;
 
 @CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/")
 public class NewsController {
+
     /*
     My private key for newAPI
      */
     static final String key = "d5cb1c65bb3442048ecba183138ceaef";
 
+    /**
+     * Endpoint required
+     * @param country_id Information from which country they are news [String]
+     * @param category_id Information about category in news [String]
+     * @return json as String
+     */
     @GetMapping("/headlines/{country_id}/{category_id}")
     public String get_top_headlines(@PathVariable("country_id") String country_id, @PathVariable("category_id") String category_id) {
         RestTemplate restTemplate = new RestTemplate();
@@ -31,20 +33,18 @@ public class NewsController {
            System.out.println(jsonResult);
            System.out.println(jsonResult.toString());
            return jsonResult.toString();
-
-//           JSONObject jsonObject = new JSONObject();
-//           jsonObject.putOpt("alldata", jsonResult);
-//           System.out.println(jsonObject);
-//           System.out.println(jsonObject.toString());
-//           return jsonObject.toString();
        }
        catch (Exception e)
         {
-            System.out.println("dupa");
-            return "{}";
+            return "{upssss....}";
         }
     }
 
+    /**
+     * TODO
+     * For develop in future
+     * @return json as String
+     */
     @GetMapping("/everything")
     public String get_everything(){
         RestTemplate restTemplate = new RestTemplate();
@@ -52,6 +52,11 @@ public class NewsController {
         return restTemplate.getForObject(url, String.class);
     }
 
+    /**
+     * TODO
+     * For develop in future
+     * @return json as String
+     */
     @GetMapping("/sources")
     public String get_sources(){
         RestTemplate restTemplate = new RestTemplate();
